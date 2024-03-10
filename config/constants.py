6f247cef854df17108d43357ea21c12128f7c327
xueying <cxueying887@gmail.com>
2024-03-10 21:41:17 +0800

    modify

--- config/constants.py
+++ config/constants.py
@@ -1,7 +1,7 @@
 DB_CONF_KEY = r".\assets\config\db_conf.key"
 DB_CONF = r".\assets\config\db_conf.yaml"
 DB_KEY = r".\assets\config\database.key"
-DB_NAME = "password_manager_dev"
+DB_NAME = "password_manager"
 TABLE_PASSWORD = "passwords"
 CONF_PATH = r".\assets\config"
 LOGS_PATH = R".\logs"
